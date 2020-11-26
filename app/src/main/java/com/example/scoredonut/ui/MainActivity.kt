f9d77ac434d9b12ffe9c0ccf77522ebe862a758d
Haris Mexis <theoharismexis@hotmail.com>
2020-11-26 21:56:59 +0000

    added network connection listener

--- app/src/main/java/com/example/scoredonut/ui/MainActivity.kt
+++ app/src/main/java/com/example/scoredonut/ui/MainActivity.kt
@@ -1,14 +1,52 @@
 package com.example.scoredonut.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import com.example.scoredonut.R
+import com.example.scoredonut.util.NetworkConnectionListener
+import com.example.scoredonut.viewmodel.MainViewModel
+import dagger.android.AndroidInjection
+import javax.inject.Inject
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NetworkConnectionListener.NetworkConnectionClient {
+
+    @Inject
+    lateinit var viewModelFactory: ViewModelProvider.Factory
+
+    private lateinit var viewModel: MainViewModel
+    private var networkListener: NetworkConnectionListener? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        viewModel = ViewModelProviders.of(this, viewModelFactory)[MainViewModel::class.java]
+        networkListener = NetworkConnectionListener(this)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        networkListener?.startListen()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        networkListener?.stopListen()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        networkListener = null
+    }
+
+    override fun onNetworkConnectionAvailable() {
+        //
+    }
+
+    override fun onNetworkConnectionLost() {
+        //
     }
 
 }
