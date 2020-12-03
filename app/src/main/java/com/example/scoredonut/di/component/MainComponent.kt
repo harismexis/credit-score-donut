e3281cdced2693176e2bed952bcceadeed9226d0
Haris Mexis <theoharismexis@hotmail.com>
2020-12-03 21:59:59 +0000

    removed commented code

--- app/src/main/java/com/example/scoredonut/di/component/MainComponent.kt
+++ app/src/main/java/com/example/scoredonut/di/component/MainComponent.kt
@@ -21,22 +21,6 @@ import javax.inject.Singleton
 )
 interface MainComponent : AndroidInjector<MainApplication> {
 
-//    @Component.Builder
-//    interface Builder {
-//
-//        /**
-//         * [BindsInstance] annotation is used for, if you want to bind particular object or instance
-//         * of an object through the time of component construction
-//         *
-//         * @BindsInstance replaces Builder appModule(AppModule appModule)
-//         * And removes Constructor with Application AppModule(Application)
-//         */
-//        @BindsInstance
-//        fun application(application: MainApplication) : Builder
-//
-//        fun build() : MainComponent
-//    }
-
     @Component.Factory
     interface Factory {
         fun create(@BindsInstance application: MainApplication) : MainComponent
